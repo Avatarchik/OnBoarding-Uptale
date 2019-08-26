a04aafa746f536a76695b1a53e485f268845c1ea
SamyAnisBenachi <samy.benachi@gmail.com>
2019-08-26 11:49:29 +0200

    Init Uptale project
    Ignore
    Uptale Project/UptaleScenes/Assets/SF_Cockpit-C2_OLD/Spaceship Cockpit_Maps.psd

---
+++ Uptale Project/UptaleScenes/Assets/CreatePNG.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class CreatePNG : MonoBehaviour
+{
+	public int imageWidth = 1024;
+	public bool saveAsJPEG = true;
+	public Camera cam;
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			byte[] bytes = I360Render.Capture(imageWidth, saveAsJPEG, cam);
+			if (bytes != null)
+			{
+				string path = Path.Combine(Application.persistentDataPath, "360render" + (saveAsJPEG ? ".jpeg" : ".png"));
+				File.WriteAllBytes(path, bytes);
+				Debug.Log("360 render saved to " + path);
+			}
+		}
+	}
+}
---
+++ Uptale Project/UptaleScenes/Assets/FlashScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class FlashScript : MonoBehaviour
+{
+
+	public Animator anim;
+
+	public GameObject Vaisseau;
+	public GameObject Lights;
+	public VideoPlayer videoPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+		StartCoroutine(Fade());
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Log("Trigger");
+			anim.SetTrigger("AnimTrigger");
+			
+			StartCoroutine(Fade());
+		}
+    }
+
+	IEnumerator Fade()
+	{
+		yield return new WaitForSeconds(8f);
+		//anim.SetTrigger("AnimTrigger");
+		yield return new WaitForSeconds(4f);
+		//Vaisseau.SetActive(true);
+		//videoPlayer.Pause();
+	}
+}
