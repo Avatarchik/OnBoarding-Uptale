51d7ccceb3519ba8dcce2b1abe802c2c3477d085
SamyAnisBenachi <samy.benachi@gmail.com>
2019-10-09 15:42:38 +0200

    Update uptale

---
+++ Uptale Project/UptaleScenes/Assets/LoadTextureSprite.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadTextureSprite : MonoBehaviour
+{
+	public Material material;
+	public Sprite sprite;
+
+    void Start()
+    {
+
+	}
+
+	private void Update()
+	{
+		/*material = this.GetComponent<Renderer>().material;
+		sprite = Resources.Load<Sprite>("SpriteTableau/sprite.png");
+		material.mainTexture = sprite.texture;*/
+	}
+
+}
